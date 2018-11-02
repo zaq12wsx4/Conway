ee8a1eba4585ede33b07840a7a778c7a67b968b1
Jonathan Finn <jonathanfinn11@gmail.com>
2018-11-02 18:49:00 +0000

    Fixed Conway

--- Conway.py
+++ Conway.py
@@ -11,14 +11,38 @@ from time import time
 # X X X X X X X X
 ######################
 
+def glider(y, x):
+    matrix[y][x+2] = 1
+    matrix[y+1][x+2] = 1
+    matrix[y+2][x+2] = 1
+    matrix[y+2][x+1] = 1
+    matrix[y+1][x] = 1
+
+def block(y, x):
+    matrix[y][x] = 1
+    matrix[y+1][x] = 1
+    matrix[y][x+1] = 1
+    matrix[y+1][x+1] = 1
+
+def spinner(y, x):
+    matrix[y][x] = 1
+    matrix[y+1][x] = 1
+    matrix[y+2][x] = 1
+
 # create a matrix the size provided by x_len and y_len
 # assign each variable in the matrix a random number, 0 or 1, to read from
-x_len, y_len = 60, 60
-matrix = [[randint(0, 1) for l in range(x_len)] for p in range(y_len)]
+x_len = int(input("Columns: "))
+y_len = int(input("Rows: "))
+
+matrix = [[0 for l in range(x_len)] for p in range(y_len)]
+
+glider(30, 2)
+
+
 
 # make a hard copy of matrix for the rest of the program to write to
 temp_matrix = [row[:] for row in matrix]
-dot_size = 6
+dot_size = 8
 
 # set animation to false
 tracer(False)
@@ -27,12 +51,50 @@ ht()
 round_count = 0
 
 # draw a new matrix of life and compute the next round
-while True:
+framecount = 2
+for frame in range(framecount):
     start_time = time()
+    clear()
+
+    square_size = dot_size
+
+    width_squares = x_len
+    height_squares = y_len
+
+    width = square_size * width_squares
+    height = square_size * height_squares
+
+
+    for each in range(int((width/square_size)/2)+1):
+        if width_squares%2 == 1:
+            each = each + 0.5
+        penup()
+        setpos(((each)*square_size)-(0.5*dot_size), (height/2)-(0.5*dot_size))
+        pendown()
+        setpos(((each)*square_size)-(0.5*dot_size), (-height/2)-(0.5*dot_size))
+        penup()
+        setpos(-((each)*square_size)-(0.5*dot_size), (height/2)-(0.5*dot_size))
+        pendown()
+        setpos(-((each)*square_size)-(0.5*dot_size), (-height/2)-(0.5*dot_size))
+
+    for each in range(int((height/square_size)/2)+1):
+        if height_squares%2 == 1:
+            each = each + 0.5
+        penup()
+        setpos((width/2)-(0.5*dot_size), ((each)*square_size)-(0.5*dot_size))
+        pendown()
+        setpos(-(width/2)-(0.5*dot_size), ((each)*square_size)-(0.5*dot_size))
+        penup()
+        setpos((width/2)-(0.5*dot_size), -((each)*square_size)-(0.5*dot_size))
+        pendown()
+        setpos(-(width/2)-(0.5*dot_size), -((each)*square_size)-(0.5*dot_size))
+        penup()
+
+
     # setup for turtle's position and clear the board
     penup()
     setpos(-x_len*(0.5*dot_size), y_len*(0.5*dot_size))
-    clear()
+
 
     # draw a dot in a position for every int in matrix
     # runs for each row
@@ -69,10 +131,7 @@ while True:
                     # try to test each position surrounding the tile, from top-left to bottom-right
                     # try in case of out of index error
                     try:
-                        if (i + y-1) < 0 or (j + x-1) < 0:
-                            raise IndexError()
-                        # print(matrix[i + y-1][j + x-1])
-                        if matrix[i + y-1][j + x-1] == 1:
+                        if matrix[(i + y-1)%y_len][(j + x-1)%x_len] == 1:
                             pos_count = pos_count + 1
                     except IndexError:
                         pass
@@ -100,7 +159,7 @@ while True:
     time_bool = True
     while time_bool:
         end_time = time()
-        if (end_time - start_time) > 0.1:
+        if (end_time - start_time) > 0.5:
             time_bool = False
     update()
 
